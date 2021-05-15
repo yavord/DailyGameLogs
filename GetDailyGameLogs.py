9d1e1bd67a71a4c0bd6ca8c4b9d605c579ac5d4e
yav-d <yavor.d0@gmail.com>
2021-05-15 14:15:33 +0300

    local files

--- GetDailyGameLogs.py
+++ GetDailyGameLogs.py
@@ -36,13 +36,14 @@ for i in range(13):
 
 
 #Get all of the links for individual game pages.
+print("Getting links for individual games")
 listOfAllLinks = []
 for links in listOfMainLinks:
     url = f"{links}"
     driver.get(url)
     sleep(5)
     src = driver.page_source
-    parser = BeautifulSoup(src, "lxml")
+    parser = BeautifulSoup(src, "html")
     tables = parser.find('tbody')
     if tables == None:
         continue
@@ -57,6 +58,7 @@ correctLinks = [a for a in listOfAllLinks if "box-score" in a]
 
 
 #go through all of the individual links to individual games and pull all of the players stats.
+print("Getting player stats")
 statsDF = pd.DataFrame()
 
 for links in correctLinks:
@@ -66,38 +68,38 @@ for links in correctLinks:
 
     for j in range(2):
  #get webpage
- src = driver.page_source
- parser = BeautifulSoup(src, "lxml")
- #find all tables on page (2)
- tables = parser.find('body').find_all('table')
- #find all headers and strip extraneous html
- headers = tables[j].findAll('th')
- headerlist = [h.text.strip() for h in headers]
- #find all rows strip first and last row
- rows = tables[j].findAll('tr')[1:]
- rows = rows[:(len(rows)-1)]
- #try to get rid of players that didn't play, but fail at it.
- properRows = [a for a in rows if "None" in a]
- #strip extraneous html and add to master dataframe
- player_stats = [[td.getText().strip() for td in rows[i].findAll('td')] for i in range(len(rows))]
- stats = pd.DataFrame(player_stats, columns=headerlist)
- index = 0
- #This will eventually have to updated with additional months. Sure I could've done this from the start
- #but I made this in january when february seemed like a lifetime away
- date = parser.find("title")
- strList = ['Jan', 'Feb', 'Dec']
- matchDate = date.text.strip()
- x = re.split("(Jan|Feb|Dec|NBA.com|2021|2020)", matchDate)
- stats['DATE'] = x[1] + x[2] + x[3]
- stats['MATCHUP'] = x[0]
+        src = driver.page_source
+        parser = BeautifulSoup(src, "html")
+        #find all tables on page (2)
+        tables = parser.find('body').find_all('table')
+        #find all headers and strip extraneous html
+        headers = tables[j].findAll('th')
+        headerlist = [h.text.strip() for h in headers]
+        #find all rows strip first and last row
+        rows = tables[j].findAll('tr')[1:]
+        rows = rows[:(len(rows)-1)]
+        #try to get rid of players that didn't play, but fail at it.
+        properRows = [a for a in rows if "None" in a]
+        #strip extraneous html and add to master dataframe
+        player_stats = [[td.getText().strip() for td in rows[i].findAll('td')] for i in range(len(rows))]
+        stats = pd.DataFrame(player_stats, columns=headerlist)
+        index = 0
+        #This will eventually have to updated with additional months. Sure I could've done this from the start
+        #but I made this in january when february seemed like a lifetime away
+        date = parser.find("title")
+        strList = ['Jan', 'Feb', 'Dec']
+        matchDate = date.text.strip()
+        x = re.split("(Jan|Feb|Dec|NBA.com|2021|2020)", matchDate)
+        stats['DATE'] = x[1] + x[2] + x[3]
+        stats['MATCHUP'] = x[0]
 
 
  #sometimes a player has their position attached to their name so remove it
- for x in stats['PLAYER']:
-     index += 1
-     if x[len(x)-1].isupper():
-         stats.loc[index-1, 'PLAYER'] = x[:-1]
- statsDF = statsDF.append(stats)
+        for x in stats['PLAYER']:
+            index += 1
+            if x[len(x)-1].isupper():
+                stats.loc[index-1, 'PLAYER'] = x[:-1]
+        statsDF = statsDF.append(stats)
 
 
 statsDF = statsDF.dropna()
@@ -108,26 +110,27 @@ statsDF.tail()
 
 # this updates the previous stats file. If you don't already have a stats file, this isn't necessary.
 # obviously has to be changed to your local repository.
-df = pd.read_excel(r"C:\Users\Matt\Desktop\ProgrammingProjects\individualStatsTest.xlsx")
+# df = pd.read_excel(r"/home/yavor/projects/PythonProjects/nba/DailyGameLogs/stats.xlsx")
 
 statsDF
 
 
 
-df
+# df
 
 
-dfres = statsDF.append(df)
+# dfres = statsDF.append(df)
 
 
 # to get rid of the list indexes created by continually appending to a previous dataframe.
 # not necessary if it's the first time running this code.
-dfres = dfres.drop(['Unnamed: 0', 'Unnamed: 0'], axis=1)
+# dfres = dfres.drop(['Unnamed: 0', 'Unnamed: 0'], axis=1)
 
 
-dfres
+# dfres
 
 
 
 # obviously change this to your local repository
-pd.DataFrame.to_excel(dfres, f"C:\\Users\\Matt\\Desktop\\ProgrammingProjects\\individualStatsTest.xlsx")
+print('writing excel')
+pd.DataFrame.to_excel(statsDF, f"/home/yavor/projects/PythonProjects/nba/DailyGameLogs/stats.xlsx")
